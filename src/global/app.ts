62bb750f0663cabf708dcb5fd2531c6729753abc
Michal-Kilian <kilian.michal.kili@gmail.com>
2025-02-18 13:16:26 +0100

    docker fix

--- src/global/app.ts
+++ src/global/app.ts
@@ -1,8 +1,6 @@
 import '@material/web/list/list'
 import '@material/web/list/list-item'
 import '@material/web/icon/icon'
-import {Config} from "@stencil/core";
 
-export const config: Config = {
-  globalScript: 'src/global/app.ts',
+export default function() {
 }
